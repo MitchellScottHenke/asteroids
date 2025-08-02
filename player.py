634e84790dd39b6cd0e70d9659df4748aac696df
MitchellScottHenke <mitch.s.henke@gmail.com>
2025-08-02 13:19:13 -0400

    Trying to get the player to turn using a and d

--- player.py
+++ player.py
@@ -1,9 +1,10 @@
 import pygame
 from circleshape import CircleShape
+from constants import PLAYER_TURN_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y, r):
-        super.__init__(x, y, r)
+        super().__init__(x, y, r)
         self.rotation = 0
         
     # in the player class
@@ -17,3 +18,15 @@ class Player(CircleShape):
     
     def draw(self, screen,):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+        
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+        
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            # If the a key is pressed, I want the player to rotate to the left
+            self.rotation(dt)
+        if keys[pygame.K_d]:
+            self.rotation(-dt)
